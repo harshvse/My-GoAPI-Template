dde806f922e84a7dfd856d57eddbf8cb3cb7dc54
Harsh Verma <harshvse@gmail.com>
2024-09-28 09:34:00 +0530

    first commit

---
+++ cmd/api/health.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) healthCheckHandler(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Ok!"))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/harshvse/go-api
+
+go 1.23.0
+
+require github.com/go-chi/chi/v5 v5.1.0 // indirect
