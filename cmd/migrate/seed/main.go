26cbeebcbf0c24484bed574bdfa56baab716f413
Harsh Verma <harshvse@gmail.com>
2024-10-22 15:36:58 +0530

    added seeder files to seed the database

---
+++ cmd/migrate/seed/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/harshvse/go-api/internal/db"
+	"github.com/harshvse/go-api/internal/store"
+)
+
+func main() {
+
+	addr := "postgres://postgres:postgres@localhost:5432/golangdb?sslmode=disable"
+	maxOpenConns := 20
+	maxIdleConns := 20
+	maxIdleTime := "15m"
+
+	conn, err := db.New(addr, maxOpenConns, maxIdleConns, maxIdleTime)
+	if err != nil {
+		log.Panic("failed to connected to database", err)
+	}
+	defer conn.Close()
+	log.Println("database connected")
+
+	store := store.NewPostgresStorage(conn)
+	db.Seed(store)
+}
---
+++ internal/db/seeder.go
@@ -0,0 +1,87 @@
+package db
+
+import (
+	"context"
+	"log"
+	"math/rand"
+
+	"github.com/jaswdr/faker/v2"
+
+	"github.com/harshvse/go-api/internal/store"
+)
+
+func Seed(store store.Storage) {
+	ctx := context.Background()
+
+	// Let's create users first
+	users := GenerateUsers(100)
+	for _, user := range users {
+		if err := store.Users.Create(ctx, user); err != nil {
+			log.Println("Error creating user: ", err)
+			return
+		}
+	}
+
+	posts := GeneratePosts(users, 100)
+	for _, post := range posts {
+		if err := store.Posts.Create(ctx, post); err != nil {
+			log.Println("Error creating posts: ", err)
+			return
+		}
+	}
+
+	comments := GenerateComments(posts, 100)
+	for _, comment := range comments {
+		if err := store.Comments.Create(ctx, comment); err != nil {
+			log.Println("Error creating comments", err)
+			return
+		}
+	}
+}
+
+func GenerateUsers(n int) []*store.User {
+	users := make([]*store.User, n)
+	fake := faker.New()
+
+	for i := 0; i < n; i++ {
+		users[i] = &store.User{
+			Username: fake.Person().Name(),
+			Email:    fake.Internet().Email(),
+			Password: fake.Internet().Password(),
+		}
+	}
+	return users
+}
+
+func GeneratePosts(users []*store.User, n int) []*store.Post {
+	posts := make([]*store.Post, n)
+	fake := faker.New()
+	for i := 0; i < n; i++ {
+		user := users[rand.Intn(len(users))]
+		tags := make([]string, 3)
+		for j := 0; j < 3; j++ {
+			tags[j] = fake.Lorem().Word()
+		}
+		posts[i] = &store.Post{
+			UserID:  user.ID,
+			Title:   fake.Lorem().Sentence(10),
+			Content: fake.Lorem().Paragraph(100),
+			Tags:    tags,
+		}
+	}
+	return posts
+}
+
+func GenerateComments(posts []*store.Post, n int) []*store.Comment {
+	comments := make([]*store.Comment, n)
+	fake := faker.New()
+	for i := 0; i < n; i++ {
+		post := posts[rand.Intn(len(posts))]
+		comments[i] = &store.Comment{
+			UserID:  post.UserID,
+			PostID:  post.ID,
+			Content: fake.Lorem().Paragraph(100),
+		}
+	}
+	return comments
+}
